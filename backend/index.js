3ce7369dfd5515e995ba02a0adbd0920e8f24fac
laureca <anynnha.laureca@gmail.com>
2020-01-14 16:23:59 -0300

    Adicção do nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros: 
+// Query Params: request.query (Filtros, ordenação, páginação, etc..)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
 app.get('/', (request, response) => {
-    return response.json('Hello Word!');
+    return response.json({ message: 'Hello OmniStack' });
 });
 
 app.listen(3333);
